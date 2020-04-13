218206aa46d02281f26e35f1ec31bd774da859ca
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-04-13 05:21:45 -0400

    4 event listener written: index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+// 1. click
+const logoHeading = document.querySelector('.logo-heading');
+
+logoHeading.addEventListener('click', (event) =>{
+   if  (event.target.style.color === 'yellow') {
+       event.target.style.color = 'black';
+   } else {
+       event.target.style.color = 'yellow';
+   } 
+})
+
+// 2. mouseenter
+const headerIMG = document.querySelector('.mouseEnter');
+headerIMG.addEventListener('mouseenter', (event) =>{
+   event.target.style.transform = 'scale(1.1)';
+   event.target.style.transition = 'transform .7s';
+})
+
+// mouseleave
+headerIMG.addEventListener('mouseleave', (event) =>{
+   event.target.style.transform = 'scale(1)';
+})
+
+// dblclick
+const doubleClick = document.querySelector('.double-click');
+
+doubleClick.addEventListener('dblclick', (event) => {
+   event.target.parentElement.style.transform = 'rotate(360deg)';
+   event.target.parentElement.style.transition = 'transform 1s';
+})
+
