52454a96fa16ea18ce71134a7eaaa68e4dfbc138
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-04-14 16:37:57 -0400

    10 unique event listeners created and added:index.js

--- js/index.js
+++ js/index.js
@@ -18,16 +18,57 @@ headerIMG.addEventListener('mouseenter', (event) =>{
    event.target.style.transition = 'transform .7s';
 })
 
-// mouseleave
+// 3. mouseleave
 headerIMG.addEventListener('mouseleave', (event) =>{
    event.target.style.transform = 'scale(1)';
 })
 
-// dblclick
+// 4. dblclick
 const doubleClick = document.querySelector('.double-click');
 
 doubleClick.addEventListener('dblclick', (event) => {
    event.target.parentElement.style.transform = 'rotate(360deg)';
    event.target.parentElement.style.transition = 'transform 1s';
 })
+
+// 5. mouseover
+const mouseOver = document.querySelector('.text-content');
+mouseOver.addEventListener('mouseover', (event)=>{
+    event.target.style.color = 'orange';
+})
+
+// 6. mouseout
+mouseOver.addEventListener('mouseout', (event) =>{
+   event.target.style.color = 'black';
+})
+
+// 7. wheel
+
+   const body = document.querySelector('body');
+   body.addEventListener('wheel', function alertMeOnce(){
+   alert("WAIT!! FASTEN YOUR SEAT BELT FIRST");
+   body.removeEventListener('wheel', alertMeOnce);
+ });
+
+// 8. keydown
+
+ const header = document.querySelector('header');
+ body.addEventListener("keydown", () => header.style.opacity = "0");
+
+ header.addEventListener("mouseenter", () => {
+   header.style.opacity = "1";
+   // TweenMax.to("header", 2, {rotation:360, scale:1.5});
+ });
+
+// 9. drag
+
+ const dragThePic = document.querySelector(".content-destination > img" );
+ dragThePic.addEventListener('drag', (event) =>{
+    event.target.style.transform = 'scale(1.5)';
+    })
+
+// 10. load
+window.addEventListener("load", function(event) {
+   alert("This is to show case the 'load' event");
+    })
 
